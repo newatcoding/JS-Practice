5657b0e3f571e6f71724945b339feefcc1cb8797
newatcoding <thakurlakshay2@gmail.com>
2021-08-23 22:57:30 +0530

    2.13 and 2,14

--- Java Script Language/JavaScript Fundamentals/2/index.js
+++ Java Script Language/JavaScript Fundamentals/2/index.js
@@ -9,3 +9,20 @@ outer: for(let i=0;i<10;i++){
     if(i>=5) break outer;
 }
 // labels doest know allow jumps
+
+/*----------------------------------------------------- 2.14 ----------------------------------------------------------------------*/
+//switches
+//without 'break' all cases after the chosen one with condition will print 
+let x=3;
+switch(x){
+    case 1:{
+        console.log("1");
+        break;
+    }case 2:{
+        console.log("2");
+        break;
+    }default:{
+        console.log("not 1 or 2");
+        break;
+    }
+}
