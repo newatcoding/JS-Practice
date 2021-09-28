39e0946ad6f881d639539853ca21e1eafd34645a
newatcoding <thakurlakshay2@gmail.com>
2021-09-28 23:03:01 +0530

    Inheritence

--- OOPS/Prototypes and inheritence/index.js
+++ OOPS/Prototypes and inheritence/index.js
@@ -33,3 +33,60 @@ function Circle(radius){
 //object created by a given constructor will have a same prototype
 const circle=new Circle(10);
 console.log(circle); //circle inherits from Circle constructor and cunstouctor inherits from baseObject, helce multilevel Inheritence
+
+//we cannot iterate over prototype beacuse they are non enumerable
+
+let objectBase=Object.getPrototypeOf(Object.getPrototypeOf(circle));
+let descriptor=Object.getOwnPropertyDescriptor(objectBase,'toString');
+console.log(descriptor) ;  //here enumerable is false meaning not visible (in loops or anything)
+
+//learn about define property in Object property Configuration
+
+//this will be used are parent proto for all object created by this constructor
+// Circle.prototype           
+
+
+//Prototype vs instance method
+function Circle1(radius){
+    this.radius=radius;
+    this.move=function(){
+        console.log('move');
+    }
+}
+const c1=new Circle1(1);      
+const c2=new Circle1(2);
+
+//2 objects have 2 copies os same function 'draw';    wastage of memory
+//solutions           use  prototypical inheritance
+//taking draw out of Circle and have it outside in another object to inherit from
+
+Circle1.prototype.draw=function(){
+    console.log('draw');
+    
+}
+console.log(c1);
+console.log(c2);
+
+Circle1.prototype.toString=function(){
+    return 'Circle with radius '+ this.radius;
+}
+
+////NOTE     INSTANCE MEANS IT'S OWN PROPERTY
+console.log(c1.toString());
+console.log(c2.toString());
+
+//iterating over instance and prototye properties
+
+console.log(Object.keys(c1));    //only returns instance members only
+
+//for in loop returns all instance AND prototypal members
+for(keys in c1){
+    console.log(keys);
+}
+
+//don't change prototype of already existing  objects , there may be a problem in future 
+Array.prototype.shuffle = function(){
+
+};
+
+const array=[];
